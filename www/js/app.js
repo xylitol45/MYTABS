f17f0ca754b1ddf13e2bf9c6e26007d79ec9cbf0
yoshimura <yoshimura@srp.co.jp>
2015-10-16 00:47:37 +0900

    init

--- www/js/app.js
+++ www/js/app.js
@@ -11,15 +11,15 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
+//    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//      cordova.plugins.Keyboard.disableScroll(true);
+//
+//    }
+//    if (window.StatusBar) {
+//      // org.apache.cordova.statusbar required
+//      StatusBar.styleLightContent();
+//    }
   });
 })
 
